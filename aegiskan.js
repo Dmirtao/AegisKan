5a34f793214f2c31fda040194eaf2cd45600b542
Andy K <Dmirtao@users.noreply.github.com>
2018-04-27 04:41:07 -0400

    Added jQuery.svg.js lib

--- aegiskan.js
+++ aegiskan.js
@@ -1,5 +1,5 @@
-SVG.on(document, 'DOMContentLoaded', function() {
+$(document).ready(function() {
+	
 
-	var draw = SVG('drawBox').size('100%','100%')
-	var rect = draw.rect(150,100).attr({fill: '#444444'})
-})
+
+}
