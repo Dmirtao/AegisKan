9cbe7d68c007debcf33324e1a57bcfbeea693816
Andy K <andykarsai@gmail.com>
2018-04-27 17:42:20 -0400

    Fixed jQuery load
    Be sure to initialize a localhost server for testing

--- aegiskan.js
+++ aegiskan.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	
+	$("button").ready(function () {
+		/* body... */
+		$("button").click(function () {
+			/* body... */
+			$("#drawBox").load("kanjivg/kanji/0966a.svg");
+		});
+	});
 
+});
 
-}
