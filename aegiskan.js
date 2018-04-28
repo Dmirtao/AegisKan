dcbb2660f0b03abc8eec077d80d6a540ecb11a5f
Andy K <Dmirtao@users.noreply.github.com>
2018-04-28 19:08:26 -0400

    Begin AJAX base

--- aegiskan.js
+++ aegiskan.js
@@ -4,9 +4,35 @@ $(document).ready(function() {
 		/* body... */
 		$("button").click(function () {
 			/* body... */
-			$("#drawBox").load("kanjivg/kanji/0966a.svg");
+			$("#drawBox").load("kanjivg/kanji/05e38.svg");
 		});
 	});
 
+
+
+
 });
 
+function unicodeToCharURL (unicodeVal) {
+	// body... 
+	var urlGen = unicodeVal;
+	return urlGen;
+}
+
+
+function getChar (url) {
+	// Argument: Unicode value for character loading.
+	var svgData = new new XMLHttpRequest();
+	svgData.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+
+		}
+	}
+	//$("svgData").load("kanjivg/kanji/05e38.svg");
+	// body... 
+}
+
+
+function makeSVGkan (argument) {
+	// body... 
+}
