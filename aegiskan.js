46dbf979a4395a691da6ef46c980b19f03eff871
Andy K <Dmirtao@users.noreply.github.com>
2018-04-28 19:33:08 -0400

    Continue AJAX XML fetchings

--- aegiskan.js
+++ aegiskan.js
@@ -1,3 +1,20 @@
+// AegisKan, by Dmirtao (Andy Karsai), 2018
+//
+// General structure of AegisKan, first Stage:
+// Webpage frontend, with Javascript running main logic. 
+// Use frontend to make AJAX queries for SVG/XML Kanji data, and store in Javascript space.
+// (An alternate future method may be loading in all of Kanji SVG data on pageload, only ~40MB )
+// Once desired SVG are in Javascript, create SVG.js objects from them as a container
+// Draw out the SVG.js objects to the webpage.
+//
+// 2nd Stage Structure:
+//
+//
+//
+
+
+
+
 $(document).ready(function() {
 	
 	$("button").ready(function () {
@@ -5,6 +22,7 @@ $(document).ready(function() {
 		$("button").click(function () {
 			/* body... */
 			$("#drawBox").load("kanjivg/kanji/05e38.svg");
+			//getChar("kanjivg/kanji/05e38.svg");
 		});
 	});
 
@@ -21,18 +39,22 @@ function unicodeToCharURL (unicodeVal) {
 
 
 function getChar (url) {
-	// Argument: Unicode value for character loading.
-	var svgData = new new XMLHttpRequest();
-	svgData.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
+	// Argument: Unicode value for character loading. Should have format kanjivg/kanji/#####.svg
+	// https://www.w3schools.com/js/js_ajax_xmlfile.asp
 
-		}
-	}
-	//$("svgData").load("kanjivg/kanji/05e38.svg");
-	// body... 
+
+	// var svgData = new XMLHttpRequest();
+	// svgData.onreadystatechange = function() {
+	// 	if (this.readyState == 4 && this.status == 200) {
+	// 		// Send addresses should be the main SVG.js canvas/object
+	// 		// ....
+	// 		document.getElementById(;="simp").innerHTML = this.
+	// 	}
+	// }
+	// svgData.open("GET", url, true);
+ //  	svgData.send();
 }
 
-
-function makeSVGkan (argument) {
-	// body... 
+function makeSVGkan (svgData) {
+	// Make SVG.js object from fetched svgData from getChar)_ 
 }
